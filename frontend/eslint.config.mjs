4d45765c51547fe574e1a808857abaaa1a9b6b16
ch00z00 <choozoo10@gmail.com>
2025-06-29 20:21:12 +0900

    Added prettier and enable that

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,10 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // next/core-web-vitals には prettier の設定が含まれていますが、
+  // 明示的に最後に prettier を指定することで、
+  // 他のルールとの競合を確実に防ぎます。
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
 ];
 
 export default eslintConfig;
