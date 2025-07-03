49d81f8908825ae52cbd49be00026a93fe39f679
ch00z00 <choozoo10@gmail.com>
2025-07-03 12:08:59 +0900

    Fixed some design issue of landing page

--- frontend/src/components/landing/LandingCTA.tsx
+++ frontend/src/components/landing/LandingCTA.tsx
@@ -5,13 +5,13 @@ import Link from 'next/link';
 export default function LandingCTA() {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-7xl px-6 py-24 text-center sm:py-32 lg:px-8">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          さあ、言語化トレーニングを始めよう。
+          さあ、言語化トレーニングを始めましょう。
           <br />
           新しい自分に出会う準備はできましたか？
         </h2>
-        <div className="mt-10 flex items-center gap-x-6">
+        <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link
             href="/themes"
             className="rounded-md bg-primary px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
